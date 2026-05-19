eb9201ab087bc489b032fe02397ab84382ec03fb
Mohan tripathi <mohantripathi90706@gmail.com>
2026-05-19 19:25:23 +0530

    NEW

---
+++ PANDAS/basics.py
@@ -0,0 +1,6 @@
+import pandas as pd
+dict1 = {"name" : ["mohan","Mehek","Urvi"],
+"Marks" : [23,53,98]}
+df = pd.DataFrame(dict1)
+# print(df)
+print(df.info())
--- dictionary/crreate_a_dict_from_user_input.py
+++ dictionary/crreate_a_dict_from_user_input.py
@@ -5,6 +5,7 @@
 #     value = input("enter the values :")
 #     d[key] = value
 
-a = {"name1" : "mohan" ,"name2" : "sohan"}
-print(a.items())
-
+a = {"zame1" : "mohan" ,"name2" : "sohan"}
+b = {"ram1" : "ssd1" , "ram2" : "ssd2"}
+print(a.pop("sohan"))
+print("let be the mohan have a fun or said that he is a villain or guy that you want in a whole world")
